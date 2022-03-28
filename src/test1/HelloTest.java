5e81d9ee81267c3004dc62fda98aafb809129722
zarina2020 <64220846+zarina2020@users.noreply.github.com>
2022-03-28 16:48:40 -0500

    Update HelloTest.java

--- src/test1/HelloTest.java
+++ src/test1/HelloTest.java
@@ -5,8 +5,7 @@ public class HelloTest {
 
         System.out.println("Hello Group-37");
 // any comment
-
-
-        // Roza add something please
     }
 }
+ public static void main(String[] args) {
+        System.out.println("Hello Mustafa, thank you ");
